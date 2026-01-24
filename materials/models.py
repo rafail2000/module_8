8ef576f2092295a296f8f39c5c9e36db2b16a3fa
rafail.shiryaev.2017 <rafail.shiryaev.2015@mail.ru>
2026-01-24 17:47:03 +0300

    add apps users and materials

---
+++ materials/models.py
@@ -0,0 +1,70 @@
+from django.db import models
+
+
+class Course(models.Model):
+    """
+    Модель курса
+    """
+
+    name = models.CharField(
+        max_length=100,
+        verbose_name="Название курса",
+        help_text="Введите название курса",
+    )
+    photo = models.ImageField(
+        upload_to="course/photo",
+        verbose_name="Фото",
+        blank=True,
+        null=True,
+        help_text="Загрузите фото",
+    )
+    description = models.TextField(
+        blank=True,
+        null=True,
+        verbose_name="описание",
+        help_text="Введите описание курса",
+    )
+
+    class Meta:
+        verbose_name = "Курс"
+        verbose_name_plural = "Курсы"
+
+    def __str__(self):
+        return self.name, self.description
+
+
+class Lesson(models.Model):
+    """
+    Модель урока
+    """
+
+    name = models.CharField(
+        max_length=100,
+        verbose_name="Название урока",
+        help_text="Введите название урока",
+    )
+    description = models.TextField(
+        blank=True,
+        null=True,
+        verbose_name="Описание урока",
+        help_text="Введите описание урока",
+    )
+    photo = models.ImageField(
+        upload_to="lesson/photo",
+        verbose_name="Фото",
+        blank=True,
+        null=True,
+        help_text="Загрузите фото",
+    )
+    link = models.CharField(
+        max_length=100,
+        verbose_name="Ссылка на видео",
+        help_text="Введите ссылку на видео",
+    )
+
+    class Meta:
+        verbose_name = "Урок"
+        verbose_name_plural = "Уроки"
+
+    def __str__(self):
+        return self.name, self.description
