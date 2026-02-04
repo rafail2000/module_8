7c79d3778829187e7515758b9f9f84e1fb6a542c
ChistyakovaVM <rafail.shiryaev.2015@mail.ru>
2026-02-04 13:51:42 +0300

    task_3

---
+++ materials/paginations.py
@@ -0,0 +1,11 @@
+from rest_framework.pagination import PageNumberPagination
+
+
+class CustomPagination(PageNumberPagination):
+    """
+    Класс пагинатор
+    """
+
+    page_size = 5
+    page_size_query_param = "page_size"
+    max_page_size = 10
--- materials/views.py
+++ materials/views.py
@@ -7,6 +7,7 @@ from rest_framework.views import APIView
 from rest_framework.viewsets import ModelViewSet
 
 from materials.models import Course, Lesson, Subscription
+from materials.paginations import CustomPagination
 from materials.serializers import (CourseDetailSerializer, CourseSerializer,
                                    LessonSerializer)
 from users.permissions import IsModerator, IsOwner
@@ -18,6 +19,7 @@ class CourseViewSet(ModelViewSet):
     """
 
     queryset = Course.objects.all()
+    pagination_class = CustomPagination
 
     def get_serializer_class(self):
         if self.action == "retrieve":
@@ -61,6 +63,7 @@ class LessonListAPIView(ListAPIView):
 
     queryset = Lesson.objects.all()
     serializer_class = LessonSerializer
+    pagination_class = CustomPagination
 
 
 class LessonRetrieveAPIView(RetrieveAPIView):
